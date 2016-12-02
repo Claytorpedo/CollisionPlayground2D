3140c1abc45d29abff51ba4e7499ba1105901201
Claytorpedo <Claytorpedo@gmail.com>
2016-12-03 00:00:44 +0900

    Removed using direction from Room.
    Touched up some comments and whitespace issues as well.

--- CollisionPlayground2D/Room.cpp
+++ CollisionPlayground2D/Room.cpp
@@ -14,8 +14,6 @@
 #include "Direction.h"
 #include "Geometry/Shape.h"
 
-using namespace direction;
-
 Room::Room() {
 	for (Uint8 r = 0; r < room::height; ++r) {
 		std::vector<Rectangle> row;
@@ -31,21 +29,21 @@ Room::~Room() {}
 
 void Room::markCollidingTiles(const Shape& collider, const units::Coordinate2D origin, const units::Coordinate2D delta) {
 	const units::Coordinate2D destination(origin + delta);
-	const Direction xDir = delta.x > 0 ? RIGHT : LEFT;
-	const Direction yDir = delta.y > 0 ? DOWN  : UP;
+	const direction::Direction xDir = delta.x > 0 ? direction::RIGHT : direction::LEFT;
+	const direction::Direction yDir = delta.y > 0 ? direction::DOWN  : direction::UP;
 	const bool isOuterAxisX = std::abs(delta.x) >= std::abs(delta.y); // We iterate first on the axis with more movement.
-	const Direction outerDir = isOuterAxisX ? xDir : yDir;
-	const Direction innerDir = isOuterAxisX ? yDir : xDir;
+	const direction::Direction outerDir = isOuterAxisX ? xDir : yDir;
+	const direction::Direction innerDir = isOuterAxisX ? yDir : xDir;
 
 	// Define incrementors for the outer and inner loops. 
 	const units::Tile outerIncr = isMax(outerDir) ? 1 : -1;
 	const units::Tile innerIncr = isMax(innerDir) ? 1 : -1;
 
-	// Starting value for the outer loop (the side of the box on the opposite side of the delta direction at the origin).
+	// Starting value for the outer loop (the side of the bounding box on the opposite side of the delta direction at the origin).
 	const units::Tile outerAxisStart = isOuterAxisX ? util::coordToTile(origin.x + collider.side(oppositeDirection(outerDir))) : 
 		                                              util::coordToTile(origin.y + collider.side(oppositeDirection(outerDir)));
-	// End value for the outer loop (the side of the box on the side of the delta direction at the destination).
-	const units::Tile outerAxisEnd =   isOuterAxisX ? util::coordToTile(destination.x + collider.side(outerDir)) : 
+	// End value for the outer loop (the side of the bounding box on the side of the delta direction at the destination).
+	const units::Tile outerAxisEnd   = isOuterAxisX ? util::coordToTile(destination.x + collider.side(outerDir)) : 
 		                                              util::coordToTile(destination.y + collider.side(outerDir));
 
 	/* To determine where to start and stop for the inner loop, we first find the ultimate starting and stopping values
@@ -59,10 +57,10 @@ void Room::markCollidingTiles(const Shape& collider, const units::Coordinate2D o
 
 	// Second axis's initial starting position (the side of the bounding box in the opposite direction of the delta direction).
 	const units::Tile innerAxisStart = isOuterAxisX ? util::coordToTile(origin.y + collider.side(oppositeDirection(innerDir))) :
-		                                              util::coordToTile(origin.x + collider.side(oppositeDirection(innerDir)));
+	                                                  util::coordToTile(origin.x + collider.side(oppositeDirection(innerDir)));
 	// Second axis's final ending position (the side of the bounding box in the delta direction at the destination).
 	const units::Tile innerAxisEnd =   isOuterAxisX ? util::coordToTile(destination.y + collider.side(innerDir)) :
-		                                              util::coordToTile(destination.x + collider.side(innerDir));
+	                                                  util::coordToTile(destination.x + collider.side(innerDir));
 
 	// Determine the slope. This is the m in y = mx + b
 	const units::Coordinate slope = isOuterAxisX ? (std::abs(delta.x) < constants::EPSILON ? 0.0f : delta.y / delta.x) : 
@@ -71,28 +69,28 @@ void Room::markCollidingTiles(const Shape& collider, const units::Coordinate2D o
 	// Determine which side of the tiles (in the array) to check for innerAxisStart and innerAxisEnd.
 	// This combined with the tiles from the outer loop lets us determine the p in y = m(p - q) + b, where p - q = x.
 	const units::Coordinate innerAxisStartTileSide = isMax(outerDir) ? 0.0f : util::tileToCoord(1);
-	const units::Coordinate innerAxisEndTileSide =   isMax(outerDir) ? util::tileToCoord(1) : 0.0f;
+	const units::Coordinate innerAxisEndTileSide   = isMax(outerDir) ? util::tileToCoord(1) : 0.0f;
 
 	// Determine the slope origins. This acts like q in y = m(p - q) + b, where p - q = x.
 	const units::Coordinate innerAxisStartOrigin = isOuterAxisX ? origin.x + collider.side(outerDir) : origin.y + collider.side(outerDir);
-	const units::Coordinate innerAxisEndOrigin =   isOuterAxisX ? origin.x + collider.side(oppositeDirection(outerDir)) : 
-		                                                          origin.y + collider.side(oppositeDirection(outerDir));
+	const units::Coordinate innerAxisEndOrigin   = isOuterAxisX ? origin.x + collider.side(oppositeDirection(outerDir)) : 
+	                                                              origin.y + collider.side(oppositeDirection(outerDir));
 
 	// Determine where to offset the slopes from. This acts like the b in y = mx + b.
 	const units::Coordinate innerAxisStartOffset = isOuterAxisX ? origin.y + collider.side(oppositeDirection(innerDir)) :
-		                                                          origin.x + collider.side(oppositeDirection(innerDir));
-	const units::Coordinate innerAxisEndOffset =   isOuterAxisX ? origin.y + collider.side(innerDir) : origin.x + collider.side(innerDir);
+	                                                              origin.x + collider.side(oppositeDirection(innerDir));
+	const units::Coordinate innerAxisEndOffset   = isOuterAxisX ? origin.y + collider.side(innerDir) : origin.x + collider.side(innerDir);
 
 	for (units::Tile i = outerAxisStart; i != outerAxisEnd + outerIncr; i += outerIncr) {
 		// Calculate the limits for the inner loop. This applies our y = mx + b (if solving for x, we essentially rotate the world and then solve for y).
-		// ..................... axis     =                    m    * ( (                   p                      )  -       q              ) +     b
+		// ..................... axis     =                    m    * ( (                   p                      ) -        q            ) +       b
 		const units::Tile calculatedStart = util::coordToTile(slope * (util::tileToCoord(i) + innerAxisStartTileSide - innerAxisStartOrigin) + innerAxisStartOffset);
 		const units::Tile calculatedEnd =   util::coordToTile(slope * (util::tileToCoord(i) + innerAxisEndTileSide   - innerAxisEndOrigin  ) + innerAxisEndOffset);
 		// Clamp values by limits.
 		const units::Tile start = isMax(innerDir) ? ( calculatedStart < innerAxisStart ? innerAxisStart : calculatedStart ) :
-			                                        ( calculatedStart > innerAxisStart ? innerAxisStart : calculatedStart);
+		                                            ( calculatedStart > innerAxisStart ? innerAxisStart : calculatedStart);
 		const units::Tile end =   isMax(innerDir) ? ( calculatedEnd > innerAxisEnd ? innerAxisEnd : calculatedEnd ) :
-			                                      ( calculatedEnd < innerAxisEnd ? innerAxisEnd : calculatedEnd );
+		                                            ( calculatedEnd < innerAxisEnd ? innerAxisEnd : calculatedEnd );
 
 		for (units::Tile k = start; k != end + innerIncr; k += innerIncr) {
 			const units::Tile row = isOuterAxisX ? k : i;

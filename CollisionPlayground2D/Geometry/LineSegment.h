c93ea623ad811807a8b0bb0f1f7af32768d4b12b
Claytorpedo <claytorpedo@gmail.com>
2017-09-12 18:44:33 -0500

    Added LineSegment.getNormalCCW() & normalized ver.

--- CollisionPlayground2D/Geometry/LineSegment.h
+++ CollisionPlayground2D/Geometry/LineSegment.h
@@ -20,6 +20,11 @@ public:
 	inline units::Coordinate max_x() const { return start.x > end.x ? start.x : end.x; }
 	inline units::Coordinate min_y() const { return start.y < end.y ? start.y : end.y; }
 	inline units::Coordinate max_y() const { return start.y > end.y ? start.y : end.y; }
+
+	// Get counter-clockwise (non-normalized) normal for the line segment. (Rotate 90 degrees CCW.)
+	inline units::Coordinate2D getNormalCCW() const { return units::Coordinate2D(start.y - end.y, end.x - start.x); }
+	// Get normalized counter-clockwise normal for the line segment. (Rotate 90 degrees CCW and normalize.)
+	inline units::Coordinate2D getNormalizedNormalCCW() const { return getNormalCCW().normalize(); }
 };
 
 #endif // _LINE_SEGMENT_H

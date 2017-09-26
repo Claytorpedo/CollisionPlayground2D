71d01a10d8d7ced9970a42dc40dd9ad7157f631d
Claytorpedo <claytorpedo@gmail.com>
2017-09-25 22:04:32 -0500

    Added SAT test with MTV output.

--- CollisionPlayground2D/Geometry/SAT.cpp
+++ CollisionPlayground2D/Geometry/SAT.cpp
@@ -57,10 +57,60 @@ inline bool _SAT(const Polygon& first, const Polygon& second, const units::Coord
 	return true;
 }
 
+// Tests the axes of one polygon against the other using SAT. Offset is the position of polygon first - second.
+// Outputs the normal and distance that make up the minimum translation vector.
+inline bool _SAT(const Polygon& first, const Polygon& second, const units::Coordinate2D& offset, units::Coordinate2D& out_norm, units::Coordinate& out_dist) {
+	const std::size_t size = first.size();
+	units::Coordinate2D axis, norm(0, 0);
+	units::Coordinate overlap1, overlap2, minDist(-1), testDist;
+	Projection projFirst, projSecond;
+	for (std::size_t i = 0; i < size; ++i) {
+		axis = first.getEdgeNorm(i); // Axis to project along.
+		projFirst = _get_projection(first, axis);
+		projSecond = _get_projection(second, axis);
+		projFirst += offset.dot(axis); // Apply offset between the two polygons' positions.
+		overlap1 = projFirst.max - projSecond.min;
+		overlap2 = projSecond.max - projFirst.min;
+		if (overlap1 < constants::EPSILON || overlap2 < constants::EPSILON)
+			return false;
+		if (projFirst.min + constants::EPSILON > projSecond.max || projFirst.max < projSecond.min + constants::EPSILON)
+			return false;
+		// Find separation for this axis.
+		testDist = overlap1 < overlap2 ? overlap1 : overlap2;
+		if (minDist == -1 || testDist < minDist) {
+			minDist = testDist;
+			norm = axis;
+		}
+	}
+	out_norm = norm;
+	out_dist = minDist;
+	return true;
+}
+
 bool sat::performSAT(const Polygon& first, const Polygon& second) {
 	return _SAT(first, second) && _SAT(second, first);
 }
 
 bool sat::performSAT(const Polygon& first, const units::Coordinate2D& firstPos, const Polygon& second, const units::Coordinate2D& secondPos) {
 	return _SAT(first, second, firstPos - secondPos) && _SAT(second, first, secondPos - firstPos);
+}
+
+bool sat::performSAT(const Polygon& first, const units::Coordinate2D& firstPos, const Polygon& second, const units::Coordinate2D& secondPos,
+                     units::Coordinate2D& out_norm, units::Coordinate& out_dist) {
+	units::Coordinate2D norm1(0, 0), norm2(0, 0);
+	units::Coordinate dist1(-1), dist2(-1);
+	if (!_SAT(first, second, firstPos - secondPos, norm1, dist1))
+		return false;
+	if (!_SAT(second, first, secondPos - firstPos, norm2, dist2))
+		return false;
+	if (dist1 < dist2) {
+		out_norm = norm1;
+		out_dist = dist1;
+	} else {
+		out_norm = norm2;
+		out_dist = dist2;
+	}
+	if (out_norm.dot(firstPos - secondPos) < 0.0f) // Ensure we're moving apart.
+		out_norm = -out_norm;
+	return true;
 }
--- CollisionPlayground2D/Geometry/SAT.h
+++ CollisionPlayground2D/Geometry/SAT.h
@@ -7,15 +7,19 @@
 class Polygon;
 
 // Separating Axis Theorem tests.
+// For all tests, "touching" polygons are not considered intersecting: they must overlap.
 namespace sat {
-	// Test if two convex polygons intersect each other.
-	// "Touching" polygons are not considered intersecting: they must overlap.
-	// Returns true if they intersect.
+	// Test if two convex polygons overlap each other.
+	// Returns true if they overlap.
 	bool performSAT(const Polygon& first, const Polygon& second);
-	// Test if two convex polygons with given positions intersect each other.
-	// "Touching" polygons are not considered intersecting: they must overlap.
-	// Returns true if they intersect.
+	// Test if two convex polygons with given positions overlap each other.
+	// Returns true if they overlap.
 	bool performSAT(const Polygon& first, const units::Coordinate2D& firstPos, const Polygon& second, const units::Coordinate2D& secondPos);
+	// Test if two convex polygons with given positions overlap each other.
+	// Gives the normal and distance that make up the minimum translation vector of separation to move the first polygon out of the second polygon.
+	// Returns true if they overlap.
+	bool performSAT(const Polygon& first, const units::Coordinate2D& firstPos, const Polygon& second, const units::Coordinate2D& secondPos,
+	                units::Coordinate2D& out_norm, units::Coordinate& out_dist);
 }
 
 #endif // _SAT_H

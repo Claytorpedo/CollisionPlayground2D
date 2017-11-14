85fc4613e37069b635b8a61ef7fe9c8288e19d56
Claytorpedo <claytorpedo@gmail.com>
2017-11-14 17:51:33 -0600

    Moved Projection to its own file.

---
+++ CollisionPlayground2D/Geometry/Projection.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef _PROJECTION_H
+#define _PROJECTION_H
+
+#include "../Units.h"
+
+// Simple struct for containing a min and max value.
+struct Projection {
+	units::Coordinate min, max;
+	Projection() : min(0), max(0) {}
+	Projection(units::Coordinate min, units::Coordinate max) : min(min), max(max) {}
+	inline Projection& operator+=(const units::Coordinate& v) { min += v; max += v; return *this; }
+};
+
+#endif // _PROJECTION_H

41091f885400b8329b9336fb6800e51fbcf9ab7b
Claytorpedo <claytorpedo@gmail.com>
2017-11-13 22:42:41 -0600

    Added colour options to DebugLogger.h
    Colours still don't work on the standard Windows
    console, so define NO_CONSOLE_COLOURS to disable them.

--- CollisionPlayground2D/Geometry/DebugLogger.h
+++ CollisionPlayground2D/Geometry/DebugLogger.h
@@ -1,9 +1,25 @@
 #pragma once
 #ifdef DEBUG
+
+/* --------------- Set logging colours --------------- */
+#ifdef NO_CONSOLE_COLOUR
+#define ERR_COL  ""
+#define WARN_COL ""
+#define LOG_COL  ""
+#define RST_COL  ""
+#else
+#define ERR_COL  "\033[31;1m"
+#define WARN_COL "\033[33;1m"
+#define LOG_COL  "\033[32;1m"
+#define RST_COL  "\033[0m"
+#endif
+/* --------------------------------------------------- */
+
 #include <iostream>
-#define DBG_ERR(x)  do { std::cerr << "\033[31;1mERROR:\033[0m "   << x << "\n"; } while (0)
-#define DBG_WARN(x) do { std::cerr << "\033[33;1mWARNING:\033[0m " << x << "\n"; } while (0)
-#define DBG_LOG(x)  do { std::cout << "\033[32;1mLOG:\033[0m "     << x << "\n"; } while (0)
+#define DBG_ERR(x)  do { std::cerr << ERR_COL  "ERROR:"   RST_COL " " << x << "\n"; } while (0)
+#define DBG_WARN(x) do { std::cerr << WARN_COL "WARNING:" RST_COL " " << x << "\n"; } while (0)
+#define DBG_LOG(x)  do { std::cout << LOG_COL  "LOG:"     RST_COL " " << x << "\n"; } while (0)
+
 #else
 #define DBG_ERR(x)  do {} while(0)
 #define DBG_WARN(x) do {} while(0)

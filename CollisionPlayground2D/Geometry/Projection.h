5b68f7a37a5d83a4c130f0f7872a634571b6d943
Claytorpedo <claytorpedo@gmail.com>
2017-12-02 18:45:58 -0600

    Added Projection, Ray, Rectangle to geom.

--- CollisionPlayground2D/Geometry/Projection.h
+++ CollisionPlayground2D/Geometry/Projection.h
@@ -1,15 +1,16 @@
 #pragma once
-#ifndef _PROJECTION_H
-#define _PROJECTION_H
+#ifndef _GEOM_PROJECTION_H
+#define _GEOM_PROJECTION_H
 
 #include "Units.h"
 
 // Simple struct for containing a min and max value.
-struct Projection {
-	units::Coordinate min, max;
-	Projection() : min(0), max(0) {}
-	Projection(units::Coordinate min, units::Coordinate max) : min(min), max(max) {}
-	inline Projection& operator+=(const units::Coordinate& v) { min += v; max += v; return *this; }
-};
-
-#endif // _PROJECTION_H
+namespace geom {
+	struct Projection {
+		gFloat min, max;
+		Projection() : min(0), max(0) {}
+		Projection(gFloat min, gFloat max) : min(min), max(max) {}
+		inline Projection& operator+=(const gFloat& v) { min += v; max += v; return *this; }
+	};
+}
+#endif // _GEOM_PROJECTION_H
--- CollisionPlayground2D/Geometry/Rectangle.cpp
+++ CollisionPlayground2D/Geometry/Rectangle.cpp
@@ -1,59 +1,61 @@
 #include "Rectangle.h"
 #include "Constants.h"
 
-inline bool Rectangle::isInside(const Rectangle& o) const {
-	return right() <= o.right() + constants::EPSILON && bottom() <= o.bottom() + constants::EPSILON &&
-	       left() >= o.left() - constants::EPSILON   && top() >= o.top() - constants::EPSILON;
-}
-
-Projection Rectangle::getProjection(const units::Coordinate2D& axis) const {
-	units::Coordinate proj(axis.dot(topLeft()));
-	units::Coordinate min(proj), max(proj);
-	proj = axis.dot(topRight());
-	if (proj < min)
-		min = proj;
-	else // Max and min are the same right now.
-		max = proj;
-	proj = axis.dot(bottomLeft());
-	if (proj < min)
-		min = proj;
-	else if (proj > max)
-		max = proj;
-	proj = axis.dot(bottomRight());
-	if (proj < min)
-		min = proj;
-	else if (proj > max)
-		max = proj;
-	return Projection(min, max);
-}
-
-units::Coordinate2D Rectangle::getClosestTo(const units::Coordinate2D point) const {
-	units::Coordinate minDist((point - topLeft()).magnitude2()), testDist;
-	units::Coordinate2D closest(topLeft());
-	testDist = (point - topRight()).magnitude2();
-	if (testDist < minDist) {
-		minDist = testDist;
-		closest = topRight();
+namespace geom {
+	inline bool Rectangle::isInside(const Rectangle& o) const {
+		return right() <= o.right() + constants::EPSILON && bottom() <= o.bottom() + constants::EPSILON &&
+			left() >= o.left() - constants::EPSILON   && top() >= o.top() - constants::EPSILON;
 	}
-	testDist = (point - bottomRight()).magnitude2();
-	if (testDist < minDist) {
-		minDist = testDist;
-		closest = bottomRight();
-	}
-	testDist = (point - bottomLeft()).magnitude2();
-	if (testDist < minDist) {
-		minDist = testDist;
-		closest = bottomLeft();
-	}
-	return closest;
-}
 
-Polygon Rectangle::toPoly() const {
-	std::vector<units::Coordinate2D> vertices;
-	vertices.reserve(4);
-	vertices.push_back(topLeft());
-	vertices.push_back(bottomLeft());
-	vertices.push_back(bottomRight());
-	vertices.push_back(topRight());
-	return Polygon(vertices);
+	Projection Rectangle::getProjection(const Coord2& axis) const {
+		gFloat proj(axis.dot(topLeft()));
+		gFloat min(proj), max(proj);
+		proj = axis.dot(topRight());
+		if (proj < min)
+			min = proj;
+		else // Max and min are the same right now.
+			max = proj;
+		proj = axis.dot(bottomLeft());
+		if (proj < min)
+			min = proj;
+		else if (proj > max)
+			max = proj;
+		proj = axis.dot(bottomRight());
+		if (proj < min)
+			min = proj;
+		else if (proj > max)
+			max = proj;
+		return Projection(min, max);
+	}
+
+	Coord2 Rectangle::getClosestTo(const Coord2 point) const {
+		gFloat minDist((point - topLeft()).magnitude2()), testDist;
+		Coord2 closest(topLeft());
+		testDist = (point - topRight()).magnitude2();
+		if (testDist < minDist) {
+			minDist = testDist;
+			closest = topRight();
+		}
+		testDist = (point - bottomRight()).magnitude2();
+		if (testDist < minDist) {
+			minDist = testDist;
+			closest = bottomRight();
+		}
+		testDist = (point - bottomLeft()).magnitude2();
+		if (testDist < minDist) {
+			minDist = testDist;
+			closest = bottomLeft();
+		}
+		return closest;
+	}
+
+	Polygon Rectangle::toPoly() const {
+		std::vector<Coord2> vertices;
+		vertices.reserve(4);
+		vertices.push_back(topLeft());
+		vertices.push_back(bottomLeft());
+		vertices.push_back(bottomRight());
+		vertices.push_back(topRight());
+		return Polygon(vertices);
+	}
 }

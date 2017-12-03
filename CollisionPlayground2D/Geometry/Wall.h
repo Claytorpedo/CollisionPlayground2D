fc226eb53c0e90d40cf9cb9aaed7042b50afd08a
Claytorpedo <claytorpedo@gmail.com>
2017-12-02 20:52:55 -0600

    Added Wall to geom.

--- CollisionPlayground2D/Geometry/Wall.h
+++ CollisionPlayground2D/Geometry/Wall.h
@@ -1,20 +1,21 @@
 #pragma once
-#ifndef _WALL_H
-#define _WALL_H
+#ifndef _GEOM_WALL_H
+#define _GEOM_WALL_H
 
 #include "Collidable.h"
 #include "ShapeContainer.h"
 
-class Wall : public Collidable {
-public:
-	Wall(const ShapeContainer& shape);
-	Wall(const ShapeContainer& shape, const units::Coordinate2D& position);
-	~Wall();
-	const units::Coordinate2D& getPosition() const;
-	const ShapeContainer& getCollider() const;
-private:
-	ShapeContainer shape_;
-	units::Coordinate2D position_;
-};
-
-#endif // _WALL_H
+namespace geom {
+	class Wall : public Collidable {
+	public:
+		Wall(const ShapeContainer& shape);
+		Wall(const ShapeContainer& shape, const Coord2& position);
+		~Wall();
+		const Coord2& getPosition() const;
+		const ShapeContainer& getCollider() const;
+	private:
+		ShapeContainer shape_;
+		Coord2 position_;
+	};
+}
+#endif // _GEOM_WALL_H

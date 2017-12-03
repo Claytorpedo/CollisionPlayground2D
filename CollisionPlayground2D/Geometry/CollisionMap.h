bc5d7d33aa68658fc46f7527d97589ffe6f8e75c
Claytorpedo <claytorpedo@gmail.com>
2017-12-02 18:22:39 -0600

    Added CollisionMap to geom.

--- CollisionPlayground2D/Geometry/CollisionMap.h
+++ CollisionPlayground2D/Geometry/CollisionMap.h
@@ -7,15 +7,16 @@
 #include "Collidable.h"
 #include "Units.h"
 
-class CollisionMap {
-public:
-	virtual ~CollisionMap() {}
-	// Given a collider and its delta, return a set of shapes it may collide with.
-	virtual const std::vector<Collidable*> getColliding(const Collidable& collider, const units::Coordinate2D& delta) const = 0;
-	// Given a collider, return a set of shapes it may overlap with.
-	virtual const std::vector<Collidable*> getColliding(const Collidable& collider) const {
-		return getColliding(collider, units::Coordinate2D(0, 0));
-	}
-};
-
+namespace geom {
+	class CollisionMap {
+	public:
+		virtual ~CollisionMap() {}
+		// Given a collider and its delta, return a set of shapes it may collide with.
+		virtual const std::vector<Collidable*> getColliding(const Collidable& collider, const Coord2& delta) const = 0;
+		// Given a collider, return a set of shapes it may overlap with.
+		virtual const std::vector<Collidable*> getColliding(const Collidable& collider) const {
+			return getColliding(collider, Coord2(0, 0));
+		}
+	};
+}
 #endif // _COLLISION_MAP_H

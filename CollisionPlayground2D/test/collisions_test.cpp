2de9a6e0aae4354a0c51995a6010fa5f0c4e7f62
Claytorpedo <claytorpedo@gmail.com>
2017-12-05 20:33:31 -0600

    Added a circle miss test to collisions_test.

--- CollisionPlayground2D/test/collisions_test.cpp
+++ CollisionPlayground2D/test/collisions_test.cpp
@@ -620,9 +620,15 @@ SCENARIO("Two shapes are separated and will not collide, detected with hybrid SA
 	}
 	GIVEN("Two circles.") {
 		Circle collider(5), stationary(2);
-		WHEN("The circles are touching, and one moves away from the other.") {
+		WHEN("The collider is on the left side of the circle, and moves leftwards, away from it.") {
 			Coord2 colliderPos(-2, 20), stationaryPos(5, 20);
 			Coord2 delta(-10, 0);
+			THEN("They don't collide.")
+				REQUIRE(geom::collides(collider, colliderPos, delta, stationary, stationaryPos, out_norm, out_t) == CollisionResult::NONE);
+		}
+		WHEN("The collider is on the right side of the circle, and moves rightwards, away from it.") {
+			Coord2 colliderPos(5, 0), stationaryPos(-2, 0);
+			Coord2 delta(10, 0);
 			THEN("They don't collide.")
 				REQUIRE(geom::collides(collider, colliderPos, delta, stationary, stationaryPos, out_norm, out_t) == CollisionResult::NONE);
 		}

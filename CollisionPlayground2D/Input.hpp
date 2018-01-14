64111971d56c760d0ef34ebd945873922072bb37
Claytorpedo <claytorpedo@gmail.com>
2018-01-14 00:28:01 -0600

    Made Input access functions const.

--- CollisionPlayground2D/Input.hpp
+++ CollisionPlayground2D/Input.hpp
@@ -28,11 +28,11 @@ public:
 
 	// Return whether a key is being held down.
 	// Note that there is no time delay for this: a key just pressed down is also held.
-	bool isKeyHeld(SDL_Keycode k);
+	bool isKeyHeld(SDL_Keycode k) const;
 	// See if a key was pressed down.
-	bool wasKeyPressed(SDL_Keycode k);
+	bool wasKeyPressed(SDL_Keycode k) const;
 	// See if a key stopped being pressed/held down.
-	bool wasKeyReleased(SDL_Keycode k);
+	bool wasKeyReleased(SDL_Keycode k) const;
 
 private:
 	std::map<SDL_Keycode, bool> held_keys_;

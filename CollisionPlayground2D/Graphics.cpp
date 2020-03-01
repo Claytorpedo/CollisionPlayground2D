06039f1a9c3af6a57a33ebca3d5a240477cc0324
Claytorpedo <claytorpedo@gmail.com>
2020-02-29 23:22:47 -0800

    Add experimental wasm support.

--- CollisionPlayground2D/Graphics.cpp
+++ CollisionPlayground2D/Graphics.cpp
@@ -6,8 +6,6 @@
 
 #include "util.hpp"
 
-const std::string Graphics::DEFAULT_WINDOW_TITLE = "Collision Playground 2D";
-
 Graphics::Graphics() : window_(nullptr), renderer_(nullptr) {}
 Graphics::~Graphics() {
 	// Free renderer and window.
@@ -19,7 +17,12 @@ bool Graphics::init(game::Pixel screenWidth, game::Pixel screenHeight) {
 	if ( !SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1") ) { // Set to linear filtering.
 		std::cout << "Warning: Linear filtering could not be enabled.\n";
 	}
-	window_ = SDL_CreateWindow(DEFAULT_WINDOW_TITLE.data(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
+	window_ = SDL_CreateWindow("Collision Playground 2D",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		screenWidth,
+		screenHeight,
+		SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 	if (!window_) {
 		std::cerr << "Error: The window could not be created.\nSDL Error: " << SDL_GetError() << "\n";
 		return false;
--- CollisionPlayground2D/game.cpp
+++ CollisionPlayground2D/game.cpp
@@ -1,116 +1,155 @@
 #include <SDL.h>
 #include <array>
 #include <iostream>
-#include <numeric>
 #include <memory>
+#include <numeric>
 #include <sstream>
+#include <string_view>
 #include <vector>
 
-#include "units.hpp"
-#include "constants.hpp"
-#include "util.hpp"
-#include "Input.hpp"
 #include "Graphics.hpp"
+#include "Input.hpp"
+
+#include "constants.hpp"
+#include "game.hpp"
 #include "generator.hpp"
+#include "units.hpp"
+#include "util.hpp"
 
 #include "geom_examples/Example.hpp"
-#include "geom_examples/ExampleShapes.hpp"
 #include "geom_examples/ExampleRays.hpp"
+#include "geom_examples/ExampleShapes.hpp"
 
 #include <Geometry2D/Geometry.hpp>
 
-namespace game {
-	const geom::Rect LEVEL_REGION = geom::Rect{ 160, 80, SCREEN_WIDTH - 320, SCREEN_HEIGHT - 160 };
-	const std::array<std::string, 7> EXAMPLE_NAMES {
-		" - Example 1: Rectangles",
-		" - Example 2: Polygons",
-		" - Example 3: Circles",
-		" - Example 4: Mixed shapes",
-		" - Example 5: Peircing ray",
-		" - Example 6: Closest ray",
-		" - Example 7: Reflecting ray",
-	};
-	const std::string WINDOW_TITLE = "Collision Playground 2D";
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
 
-	void close() {
-		SDL_Quit();
+using namespace game;
+
+namespace {
+const geom::Rect LEVEL_REGION = geom::Rect{160, 80, SCREEN_WIDTH - 320, SCREEN_HEIGHT - 160};
+constexpr std::array<std::string_view, 7> EXAMPLE_NAMES{
+	" - Example 1: Rectangles",
+	" - Example 2: Polygons",
+	" - Example 3: Circles",
+	" - Example 4: Mixed shapes",
+	" - Example 5: Peircing ray",
+	" - Example 6: Closest ray",
+	" - Example 7: Reflecting ray",
+};
+constexpr std::string_view WINDOW_TITLE = "Collision Playground 2D";
+
+Input input;
+Graphics graphics;
+MS previousTime = 0;
+MS elapsedTime = 0;
+std::unique_ptr<Example> example;
+std::size_t exampleNum = 0;
+
+void close() {
+	SDL_Quit();
+}
+
+void closeWithError() {
+	std::cout << "Press Enter to close." << std::endl;
+	std::cin.ignore();
+	close();
+}
+
+constexpr int FPS_SMOOTHING = 20; // Get an average over several frames.
+std::vector<game::FPS> fps_counter_;
+std::string getFPS() {
+	if (fps_counter_.size() >= FPS_SMOOTHING)
+		fps_counter_.erase(fps_counter_.begin());
+	fps_counter_.emplace_back(util::millisToFPS(elapsedTime));
+
+	game::FPS ave = std::accumulate(fps_counter_.cbegin(), fps_counter_.cend(), 0);
+	std::ostringstream stream;
+	stream << "FPS: " << ave / fps_counter_.size() << " - ";
+	return stream.str();
+}
+
+#ifdef __EMSCRIPTEN__
+void
+#else
+bool
+#endif
+update() {
+
+	if (!input.refresh() || input.wasKeyPressed(SDLK_ESCAPE)) {
+#ifdef __EMSCRIPTEN__
+		emscripten_cancel_main_loop();
+		return;
+#else
+		return false;
+#endif
 	}
 
-	void closeWithError() {
-		std::cout << "Press Enter to close." << std::endl;
-		std::cin.ignore();
-		close();
+	if (input.wasKeyPressed(SDLK_r)) {
+		example->reset();
+	} else if (input.wasKeyPressed(SDLK_1)) {
+		example = std::make_unique<ExampleShapes>(ExampleShapes::ExampleType::RECT, LEVEL_REGION);
+		exampleNum = 0;
+	} else if (input.wasKeyPressed(SDLK_2)) {
+		example = std::make_unique<ExampleShapes>(ExampleShapes::ExampleType::POLY, LEVEL_REGION);
+		exampleNum = 1;
+	} else if (input.wasKeyPressed(SDLK_3)) {
+		example = std::make_unique<ExampleShapes>(ExampleShapes::ExampleType::CIRCLE, LEVEL_REGION);
+		exampleNum = 2;
+	} else if (input.wasKeyPressed(SDLK_4)) {
+		example = std::make_unique<ExampleShapes>(ExampleShapes::ExampleType::MIXED, LEVEL_REGION);
+		exampleNum = 3;
+	} else if (input.wasKeyPressed(SDLK_5)) {
+		example = std::make_unique<ExampleRays>(ExampleRays::ExampleType::PEIRCING, LEVEL_REGION);
+		exampleNum = 4;
+	} else if (input.wasKeyPressed(SDLK_6)) {
+		example = std::make_unique<ExampleRays>(ExampleRays::ExampleType::CLOSEST, LEVEL_REGION);
+		exampleNum = 5;
+	} else if (input.wasKeyPressed(SDLK_7)) {
+		example = std::make_unique<ExampleRays>(ExampleRays::ExampleType::REFLECTING, LEVEL_REGION);
+		exampleNum = 6;
 	}
 
-	constexpr int FPS_SMOOTHING = 20; // Get an average over several frames.
-	std::vector<game::FPS> fps_counter_;
-	std::string getFPS(const game::MS elapsedTime) {
-		if (fps_counter_.size() >= FPS_SMOOTHING)
-			fps_counter_.erase(fps_counter_.begin());
-		fps_counter_.emplace_back(util::millisToFPS(elapsedTime));
+	MS currentTime = SDL_GetTicks();
+	elapsedTime = currentTime - previousTime;
+	previousTime = currentTime;
+	example->update(input, elapsedTime);
 
-		game::FPS ave = std::accumulate(fps_counter_.cbegin(), fps_counter_.cend(), 0);
-		std::ostringstream stream;
-		stream << "FPS: " << ave / fps_counter_.size() << " - ";
-		return stream.str();
+	graphics.clear(game::BACKGROUND_COLOUR);
+	example->draw(graphics);
+	std::string windowTitle = getFPS();
+	windowTitle.reserve(windowTitle.size() + WINDOW_TITLE.size() + EXAMPLE_NAMES[exampleNum].size());
+	windowTitle += WINDOW_TITLE;
+	windowTitle += EXAMPLE_NAMES[exampleNum];
+	graphics.setWindowTitle(windowTitle);
+	graphics.present();
+
+#ifndef __EMSCRIPTEN__
+	return true;
+#endif
+}
+} // namespace
+
+int game::run(int, char* []) {
+	gen::init();
+
+	if (!graphics.init(game::SCREEN_WIDTH, game::SCREEN_HEIGHT)) {
+		std::cerr << "Error: Failed to initialize graphics.\n";
+		closeWithError();
+		return -1;
 	}
 
-	int run(int, char*[]) {
-		Input input;
-		Graphics graphics;
-		game::MS currentTime, previousTime, elapsedTime;
-		gen::init();
-
-		if (!graphics.init(game::SCREEN_WIDTH, game::SCREEN_HEIGHT)) {
-			std::cerr << "Error: Failed to initialize graphics.\n";
-			closeWithError();
-			return -1;
-		}
-		std::unique_ptr<Example> example(std::make_unique<ExampleShapes>(ExampleShapes::ExampleType::MIXED, LEVEL_REGION));
-		uint32_t exampleNum{ 4 };
-		previousTime = SDL_GetTicks();
-		// Start the game loop.
-		while (true) {
-			if (!input.refresh())
-				break; // Window was closed.
-			if (input.wasKeyPressed(SDLK_ESCAPE))
-				break;
-			if (input.wasKeyPressed(SDLK_r)) {
-				example->reset();
-			} else if (input.wasKeyPressed(SDLK_1)) {
-				example = std::make_unique<ExampleShapes>(ExampleShapes::ExampleType::RECT, LEVEL_REGION);
-				exampleNum = 0;
-			} else if (input.wasKeyPressed(SDLK_2)) {
-				example = std::make_unique<ExampleShapes>(ExampleShapes::ExampleType::POLY, LEVEL_REGION);
-				exampleNum = 1;
-			} else if (input.wasKeyPressed(SDLK_3)) {
-				example = std::make_unique<ExampleShapes>(ExampleShapes::ExampleType::CIRCLE, LEVEL_REGION);
-				exampleNum = 2;
-			} else if (input.wasKeyPressed(SDLK_4)) {
-				example = std::make_unique<ExampleShapes>(ExampleShapes::ExampleType::MIXED, LEVEL_REGION);
-				exampleNum = 3;
-			} else if (input.wasKeyPressed(SDLK_5)) {
-				example = std::make_unique<ExampleRays>(ExampleRays::ExampleType::PEIRCING, LEVEL_REGION);
-				exampleNum = 4;
-			} else if (input.wasKeyPressed(SDLK_6)) {
-				example = std::make_unique<ExampleRays>(ExampleRays::ExampleType::CLOSEST, LEVEL_REGION);
-				exampleNum = 5;
-			} else if (input.wasKeyPressed(SDLK_7)) {
-				example = std::make_unique<ExampleRays>(ExampleRays::ExampleType::REFLECTING, LEVEL_REGION);
-				exampleNum = 6;
-			}
-
-			currentTime = SDL_GetTicks();
-			elapsedTime = currentTime - previousTime;
-			previousTime = currentTime;
-			example->update(input, elapsedTime);
-
-			graphics.clear(game::BACKGROUND_COLOUR);
-			example->draw(graphics);
-			graphics.setWindowTitle(getFPS(elapsedTime) + WINDOW_TITLE + EXAMPLE_NAMES[exampleNum]);
-			graphics.present();
-		}
-		close();
-		return 0;
-	}
+	example = std::make_unique<ExampleShapes>(ExampleShapes::ExampleType::MIXED, LEVEL_REGION);
+	exampleNum = 4;
+	previousTime = SDL_GetTicks();
+	// Start the game loop.
+#ifdef __EMSCRIPTEN__
+	emscripten_set_main_loop(update, -1, 1);
+#else
+	while (update());
+#endif
+	close();
+	return 0;
 }
